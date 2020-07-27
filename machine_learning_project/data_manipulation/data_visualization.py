f9c062994235726c426686b3085495bd5179b43d
micheleantonazzi <micheleantonazzi@gmail.com>
2020-07-27 14:59:35 +0200

    data_visualization.py: added method to visualize the dataset's images

--- machine_learning_project/data_manipulation/data_visualization.py
+++ machine_learning_project/data_manipulation/data_visualization.py
@@ -1,2 +1,28 @@
+import numpy
+
+from machine_learning_project.data_manipulation.data_retrieval import DataRetrieval
+import matplotlib.pyplot as plt
+
+
 class DataVisualization:
-    pass
+    def __init__(self, data_retrieval: DataRetrieval):
+        self._data_retrieval = data_retrieval
+
+    def print_original_images(self):
+        shuffled_indices = numpy.random.permutation(len(self._data_retrieval.get_labels()))
+        train_set, _ = self._data_retrieval.create_tensorflow_dataset(shuffled_indices, numpy.array([]))
+        iterator = iter(train_set)
+
+        plt.figure(figsize=(10, 7))
+        for i in range(10):
+            while True:
+                image, label = next(iterator)
+                print(image)
+                if label == i:
+                    ax = plt.subplot(2, 5, i + 1)
+                    plt.imshow(image.numpy().astype("uint8"))
+                    plt.title(self._data_retrieval.get_categories()[i].title())
+                    plt.axis("off")
+                    break
+        plt.show()
+
