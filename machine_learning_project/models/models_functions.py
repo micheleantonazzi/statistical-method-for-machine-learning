1540e926991c5cc6de9d9a2abe4789c477bce5f9
micheleantonazzi <micheleantonazzi@gmail.com>
2020-07-28 18:04:28 +0200

    cnn_2.py: added new model

--- machine_learning_project/models/models_functions.py
+++ machine_learning_project/models/models_functions.py
@@ -1,3 +1,4 @@
 from machine_learning_project.models.cnn_1 import cnn_1
+from machine_learning_project.models.cnn_2 import cnn_2
 
-MODELS_FUNCTIONS = {'CNN_1': cnn_1}
+MODELS_FUNCTIONS = {'CNN_1': cnn_1, 'CNN_2': cnn_2}
