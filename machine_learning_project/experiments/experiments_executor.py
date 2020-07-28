221c9352a3e019ea6a63f4cf78281abf9abadcf4
micheleantonazzi <micheleantonazzi@gmail.com>
2020-07-28 18:39:41 +0200

    results.py: create new class to store and manipulate results

--- machine_learning_project/experiments/experiments_executor.py
+++ machine_learning_project/experiments/experiments_executor.py
@@ -3,10 +3,9 @@ from tensorflow.python.data.ops.dataset_ops import AUTOTUNE
 from tqdm import tqdm
 
 from machine_learning_project.data_manipulation.data_retrieval import DataRetrieval
-from machine_learning_project.experiments.results import save_results
+from machine_learning_project.experiments.results import Results
 from machine_learning_project.models.models_functions import MODELS_FUNCTIONS
-import pandas
-from sanitize_ml_labels import sanitize_ml_labels
+
 
 from machine_learning_project.models.models_parameters import MODELS_PARAMETERS
 
@@ -28,10 +27,10 @@ class ExperimentsExecutor:
         self._test_set_size = test_set_size
 
     def execute_experiment(self):
+        results = Results()
         images, labels = self._data.load_dataset()
         splits = StratifiedShuffleSplit(n_splits=self._holdouts, test_size=self._test_set_size, random_state=42)
 
-        results = []
         for i, (train_index, test_index) in tqdm(enumerate(splits.split(images, labels)), total=self._holdouts, desc="Computing holdouts", dynamic_ncols=True):
             train_set, test_set = self._data.create_tensorflow_dataset(train_index, test_index, self._preprocessing_pipeline)
 
@@ -48,30 +47,7 @@ class ExperimentsExecutor:
                 model = model_function()
                 history = model.fit(train_set_opt, validation_data=test_set_opt, epochs=parameters['epochs']).history
 
-                # Take the metrics of last epoch both for training and test
-                scores = pandas.DataFrame(history).iloc[-1].to_dict()
-                model_results.append({
-                    'model': model_name,
-                    'run_type': 'train',
-                    'holdout': i,
-                    **{
-                        sanitize_ml_labels(key): value
-                        for key, value in scores.items()
-                        if not key.startswith("val_")
-                    }
-                })
-                model_results.append({
-                    "model": model_name,
-                    "run_type": "test",
-                    "holdout": i,
-                    **{
-                        sanitize_ml_labels(key[4:]): value
-                        for key, value in scores.items()
-                        if key.startswith("val_")
-                    }
-                })
-
-                save_results(model_results, self._preprocessing_pipeline.__name__, i, model_name)
-                results = results + model_results
+                # Extract the resulting metric and save them
+                results.extract_holdout_results(history, self._preprocessing_pipeline.__name__, i, model_name)
 
 
--- machine_learning_project/experiments/results.py
+++ machine_learning_project/experiments/results.py
@@ -1,13 +1,48 @@
 import os
 import pickle
+import pandas
+from sanitize_ml_labels import sanitize_ml_labels
 
 
-def save_results(results, preprocessing_pipeline: str, holdout_number: int, model_name: str):
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results', preprocessing_pipeline,
-                        'holdout_' + str(holdout_number))
-    if not os.path.exists(path):
-        os.makedirs(path)
+class Results:
+    def __init__(self):
+        self._results = []
 
-    path = os.path.join(path, model_name + '.pkl')
-    with open(os.path.join(path), 'wb') as f:
-        pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
+    def extract_holdout_results(self, history, preprocessing_pipeline: str, holdout_number: int, model_name: str):
+        model_results = []
+
+        scores = pandas.DataFrame(history).iloc[-1].to_dict()
+        model_results.append({
+            'model': model_name,
+            'run_type': 'train',
+            'holdout': holdout_number,
+            **{
+                sanitize_ml_labels(key): value
+                for key, value in scores.items()
+                if not key.startswith("val_")
+            }
+        })
+        model_results.append({
+            "model": model_name,
+            "run_type": "test",
+            "holdout": holdout_number,
+            **{
+                sanitize_ml_labels(key[4:]): value
+                for key, value in scores.items()
+                if key.startswith("val_")
+            }
+        })
+
+        self._results = self._results + model_results
+        self.save_results(model_results, preprocessing_pipeline, holdout_number, model_name)
+
+    def save_results(self, results, preprocessing_pipeline: str, holdout_number: int, model_name: str):
+
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results', preprocessing_pipeline,
+                            'holdout_' + str(holdout_number))
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        path = os.path.join(path, model_name + '.pkl')
+        with open(os.path.join(path), 'wb') as f:
+            pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
