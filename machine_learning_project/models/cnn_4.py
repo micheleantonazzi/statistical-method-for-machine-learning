c9334e290e2b533a6f7af5cddb2e3271e21d1237
micheleantonazzi <m7242872428>
2020-07-29 12:00:08 +0200

    cnn_4.py: added new model

---
+++ machine_learning_project/models/cnn_4.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.keras.metrics import Accuracy
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
+
+
+def cnn_4():
+    model = tf.keras.Sequential([
+        Input(shape=(32, 32, 3)),
+        Conv2D(8, 2, activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(4, 2, activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(2, 2, activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(10)
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'],
+    )
+
+    return model
+
+
+def cnn_4_parameters():
+    return {'epochs': 100, 'batch_size': 32}
--- machine_learning_project/models/models_functions.py
+++ machine_learning_project/models/models_functions.py
@@ -1,6 +1,7 @@
 from machine_learning_project.models.cnn_1 import cnn_1, cnn_1_parameters
 from machine_learning_project.models.cnn_2 import cnn_2, cnn_2_parameters
 from machine_learning_project.models.cnn_3 import cnn_3_parameters, cnn_3
+from machine_learning_project.models.cnn_4 import cnn_4, cnn_4_parameters
 
-MODELS_FUNCTIONS = {'CNN_1': cnn_1, 'CNN_2': cnn_2, 'CNN_3': cnn_3}
-MODELS_PARAMETERS = {'CNN_1': cnn_1_parameters(), 'CNN_2': cnn_2_parameters(), 'CNN_3': cnn_3_parameters()}
+MODELS_FUNCTIONS = {'CNN_1': cnn_1, 'CNN_2': cnn_2, 'CNN_3': cnn_3, 'CNN_4': cnn_4}
+MODELS_PARAMETERS = {'CNN_1': cnn_1_parameters(), 'CNN_2': cnn_2_parameters(),'CNN_3': cnn_3_parameters(), 'CNN_4': cnn_4_parameters()}
