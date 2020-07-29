f9d71141eeebabc2b65ec5468f869926fd5362a5
micheleantonazzi <m7242872428>
2020-07-29 12:00:31 +0200

    models_functions.py: fixed typo

--- machine_learning_project/models/models_functions.py
+++ machine_learning_project/models/models_functions.py
@@ -4,4 +4,4 @@ from machine_learning_project.models.cnn_3 import cnn_3_parameters, cnn_3
 from machine_learning_project.models.cnn_4 import cnn_4, cnn_4_parameters
 
 MODELS_FUNCTIONS = {'CNN_1': cnn_1, 'CNN_2': cnn_2, 'CNN_3': cnn_3, 'CNN_4': cnn_4}
-MODELS_PARAMETERS = {'CNN_1': cnn_1_parameters(), 'CNN_2': cnn_2_parameters(),'CNN_3': cnn_3_parameters(), 'CNN_4': cnn_4_parameters()}
+MODELS_PARAMETERS = {'CNN_1': cnn_1_parameters(), 'CNN_2': cnn_2_parameters(), 'CNN_3': cnn_3_parameters(), 'CNN_4': cnn_4_parameters()}
