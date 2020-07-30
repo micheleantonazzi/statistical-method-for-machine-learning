274fde905da90d043029beb2ceb06b99229cce95
micheleantonazzi <m7242872428>
2020-07-30 14:55:29 +0200

    cnn_2.py, cnn_3.py, cnn_4.py: modified architecture

--- machine_learning_project/models/cnn_2.py
+++ machine_learning_project/models/cnn_2.py
@@ -7,9 +7,9 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 def cnn_2():
     model = tf.keras.Sequential([
         Input(shape=(32, 32, 3)),
-        Conv2D(4, 2, activation='relu'),
+        Conv2D(4, (2, 2), activation='relu'),
         MaxPooling2D(2, 2),
-        Conv2D(2, 2, activation='relu'),
+        Conv2D(2, (2, 2), activation='relu'),
         MaxPooling2D(2, 2),
         Flatten(),
         Dense(64, activation='relu'),
--- machine_learning_project/models/cnn_4.py
+++ machine_learning_project/models/cnn_4.py
@@ -7,11 +7,11 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 def cnn_4():
     model = tf.keras.Sequential([
         Input(shape=(32, 32, 3)),
-        Conv2D(8, 2, activation='relu'),
+        Conv2D(8, (2, 2), activation='relu'),
         MaxPooling2D(2, 2),
-        Conv2D(4, 2, activation='relu'),
+        Conv2D(4, (2, 2), activation='relu'),
         MaxPooling2D(2, 2),
-        Conv2D(2, 2, activation='relu'),
+        Conv2D(2, (2, 2), activation='relu'),
         MaxPooling2D(2, 2),
         Flatten(),
         Dense(64, activation='relu'),
